a2593ffc3717016be823fd03850e806ada9b56b8
Darlene-uxui <darlene.uxui@gmail.com>
2022-08-23 15:47:17 +1200

    Started styling frontend

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
-<script setup>
-</script>
-
 <template>
-  <header>
+<div class="grid-container">
+      <header>
+        <h1>Pineapple Pizza</h1>
+      </header>
+      <nav></nav>
+      <main>
+        <h2>Upload Your Pizza</h2>
 
-  </header>
+        <img src="https://i.pinimg.com/474x/2b/e3/ac/2be3acf14297100ea06ca2e1ec5c1443.jpg" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptas
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptas
 
-  <RouterView />
+        </p>
+      </main>
+      <aside>
+      </aside>
+      <footer>
+        <p>Formative by Lingyi, Grace, Darlene, Jannah</p>
+      </footer>
+    </div>
+
+
 </template>
 
-<style scoped>
+<style >
 
 </style>
+
+<script>
+  import { createApp } from 'vue'
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+</script>
+
