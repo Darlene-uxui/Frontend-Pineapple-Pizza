2b599db0b8ee1bcbb8a325f1fa1f4786280c7190
Sujung Yeo <sujungyeo@Sujungui-MacBookPro.local>
2022-08-23 16:25:27 +1200

    adding PizzaListItem.vue and adding code to app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,25 @@
       <nav></nav>
       <main>
         <h2>Upload Your Pizza</h2>
+        <form>
+<div class="form-group">
+    <label for="title">title</label>
+    <input type="text" v-model="title" id="title">
+</div>
+<div class="form-group">
+    <label for="description">description</label>
+    <input type="text" v-model="description" id="description">
+</div>
+<div class="form-group">
+    <label for="nickName">nickName</label>
+    <input type="text" v-model="nickname" id="nickName">
+</div>
+<div class="form-group">
+    <label for="imageURL">image URL</label>
+    <input type="url" v-model="imageurl" id="imageURL">
+</div>
+<button @click="submitForm" type="button">Create my Pizza</button>
+</form>
 
         <img src="https://i.pinimg.com/474x/2b/e3/ac/2be3acf14297100ea06ca2e1ec5c1443.jpg" />
         <p>
@@ -31,14 +50,55 @@
 </style>
 
 <script>
-  import { createApp } from 'vue'
+import PizzaListItem from './components/PizzaListItem.vue';
 
-  createApp({
+  export default {
+    components: {PizzaListItem},
     data() {
       return {
-        message: 'Hello Vue!'
-      }
+        title: null,
+        description: null,
+        nickname: null,
+        imageurl: null,
+        pizzasArray: [],
+      };
+    },
+    methods: {
+      async submitForm() {
+        console.log("submit form");
+        const response = await fetch("http://localhost:3000/pizza", {
+          method: "POST",
+          headers: { "content-type":"application/json" },
+          body: JSON.stringify({
+            title: this.title,
+            description: this.description,
+            nickname: this.nickname,
+            imageurl: this.imageurl
+          })
+        });
+        const data = await response.text();
+        console.log(data);
+        this.getPizzas();
+      },
+
+      async getPizzas() {
+        const response = await fetch("http://localhost:3000/pizza");
+        const data = await response.json();
+        this.pizzasArray = data;
+      },
+      
+      async deletePizza(id) {
+        const response = await fetch(`http://localhost:3000/pizza/${id}`, {
+        method: "DELETE"
+        });
+        const data = await response.text();
+
+        this.getPizzas();
+        }
+    },
+    mounted() {
+      this.getPizzas();
     }
-  }).mount('#app')
+  }
 </script>
 
---
+++ src/components/PizzaListItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="pizza">
+        <div class="pizza-box">
+            <p class="title">Title:{{ pizzaData.title }}</p>
+            <p class="description">Description:{{ pizzaData.description }}</p>
+            <p class="nickName">Nick Name:{{ pizzaData.nickname }}</p>
+            <img class="image" :src="pizzaData.imageurl" alt="pizza image" />
+        </div>
+    </div>
+            <button class="delete-btn" @click="$emit('deletePizza', pizzaData.id)" type="button">Clear</button>
+</template>
+
+<script>
+    export default {
+        props: {
+            pizzaData: Object,
+        },
+        data() {
+            return {
+                title: this.pizzaData.title,
+                description: this.pizzaData.description,
+                nickname: this.pizzaData.nickname,
+                imageurl: this.pizzaData.imageurl
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+.image {
+    height: 200px;
+    width: 300px;
+}
+</style>
